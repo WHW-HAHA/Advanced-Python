4de5700653de090ed6f6213126d8b2494edca4f4
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-06-30 13:52:52 +0200

    updates

---
+++ Tutorial_3/Python_built_in.py
@@ -0,0 +1,10 @@
+# hashable, the variable has unique address
+
+# __format__ built in
+
+class student():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    
