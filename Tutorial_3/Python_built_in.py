19587f4e738d1ea213f23a2d8e909913124a91da
UniCorn <43483189+WHW-HAHA@users.noreply.github.com>
2019-06-30 14:12:00 +0200

    updates

--- Tutorial_3/Python_built_in.py
+++ Tutorial_3/Python_built_in.py
@@ -66,3 +66,22 @@ events = Events([
 if not events.is_empty():
     print(events.list_events_by_range(1, 3))
 
+
+class Events:
+    def __init__(self, events):
+        self.events = events
+
+    def __len__(self):
+        """自定义长度，将会被用来做布尔判断"""
+        return len(self.events)
+
+    def __getitem__(self, index):
+        """自定义切片方法"""
+        # 直接将 slice 切片对象透传给 events 处理
+        return self.events[index]
+
+
+# 判断是否有内容，打印第二个和第三个对象
+if events:
+    print(events[1:3])
+
